810ddbbd9da379c08d2020462fc047c4d349edf0
Ryan <rwmoskaluk@gmail.com>
2021-10-17 18:51:46 -0400

    updated test_slam and added rviz config

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -9,7 +9,8 @@ else
   term_name="gnome-terminal -- bash -c"
 fi
 
-map_path="~/catkin_ws/src/map"
+map_path="$HOME/catkin_ws/src/map"
+rviz_path="$HOME/catkin_ws/src/rvizConfig"
 
 $term_name "killall gzserver && killall gazebo " &
 sleep 1
@@ -22,7 +23,7 @@ $term_name " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 
 sleep 5
 
-$term_name " roslaunch turtlebot_interactions turtlebot_rviz_launches rviz_config_file=~/catkin_ws/src/config/rviz.config " &
+$term_name " roslaunch turtlebot_interactions turtlebot_rviz_launches rviz_config_file=$rviz_path/rviz.rviz " &
 
 sleep 5
 
