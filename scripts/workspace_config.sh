c21fa20fa1e70490b2001e3cd3ffdceb229f826e
Ryan <rwmoskaluk@gmail.com>
2021-10-17 18:11:04 -0400

    created Udacity workspace configuration script

--- scripts/workspace_config.sh
+++ scripts/workspace_config.sh
@@ -21,6 +21,29 @@
 #source devel/setup.bash
 
 
-# check if ~/catkin_ws/src exists, if it doesn't exist create it
+$(mkdir -p ~/catkin_ws/src)
 
-#
+$(cd ..)
+
+$(catkin_make)
+
+$(sudo apt-get update)
+
+$(cd ~/catkin_ws/src)
+
+$(git clone https://github.com/ros-perception/slam_gmapping)
+$(git clone https://github.com/turtlebot/turtlebot)
+$(git clone https://github.com/turtlebot/turtlebot_interactions)
+$(git clone https://github.com/turtlebot/turtlebot_simulator)
+
+$(cd ~/catkin_ws/)
+$(source devel/setup.bash)
+
+$(rosdep -i install gmapping)
+$(rosdep -i install turtlebot_teleop)
+$(rosdep -i install turtlebot_rviz_launchers)
+$(rosdep -i install turtlebot_gazebo)
+
+$(catkin_make)
+$(source devel/setup.bash)
+echo "completed workspace configuration"
