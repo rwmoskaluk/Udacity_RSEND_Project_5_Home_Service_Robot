ae9a13ff9edfaedd5490ec4a27ae168a79a00e65
Ryan <rwmoskaluk@gmail.com>
2021-10-17 18:29:01 -0400

    verified workspace script

--- scripts/workspace_config.sh
+++ scripts/workspace_config.sh
@@ -28,21 +28,25 @@ else
   term_name="gnome-terminal -- bash -c"
 fi
 
-$term_name " mkdir -p ~/catkin_ws/src &&
-cd ~/catkin_ws/ &&
-catkin_make &&
-sudo apt-get update &&
-cd ~/catkin_ws/src &&
+mkdir -p ~/catkin_ws/src
+
+(cd ~/catkin_ws/src && catkin_init_workspace)
+
+(cd ~/catkin_ws/ && catkin_make)
+sudo apt-get update
+(cd ~/catkin_ws/src &&
 git clone https://github.com/ros-perception/slam_gmapping &&
 git clone https://github.com/turtlebot/turtlebot &&
 git clone https://github.com/turtlebot/turtlebot_interactions &&
-git clone https://github.com/turtlebot/turtlebot_simulator &&
-cd ~/catkin_ws/ &&
+git clone https://github.com/turtlebot/turtlebot_simulator)
+
+(cd ~/catkin_ws/ &&
 source devel/setup.bash &&
 rosdep -i install gmapping &&
 rosdep -i install turtlebot_teleop &&
 rosdep -i install turtlebot_rviz_launchers &&
 rosdep -i install turtlebot_gazebo &&
 catkin_make &&
-source devel/setup.bash "
+source devel/setup.bash)
+
 echo "completed workspace configuration"
